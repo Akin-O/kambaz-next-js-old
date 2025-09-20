bd3af6c40e5f6cedebf58bbcd6659f17dee1020e
Akin-O <148026745+Akin-O@users.noreply.github.com>
2025-09-19 22:41:29 -0400

    Test for error fix #2

--- src/app/(Kambaz)/Courses/Assignments/page.tsx
+++ src/app/(Kambaz)/Courses/Assignments/page.tsx
@@ -2,18 +2,18 @@ export default function Assignments() {
   return (
     <div id="wd-assignments">
       <input placeholder="Search for Assignments"
-             id="wd-search-assignment" />
+        id="wd-search-assignment" />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
       <h3 id="wd-assignments-title">
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
-            A1 - ENV + HTML
-          </link> </li>
+          <link href="/Courses/1234/Assignments/123" className="wd-assignment-link"></link> </li>
         <li className="wd-assignment-list-item">
+          {/* Complete On Your Own */}
         </li>
       </ul>
     </div>
-);}
+  );
+}
